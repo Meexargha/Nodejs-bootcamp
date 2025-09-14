dab0a9bd5edcf618742d888527c52b3ad9cd8e3d
Argha kundu <128323651+Meexargha@users.noreply.github.com>
2025-09-14 18:08:35 +0530

    middleware.js

--- middleware.js
+++ middleware.js
@@ -1,1 +1,12 @@
+//hotel managemnt {loging}-user hit the login when user came and the url is hited 
 
+//middleware function 
+const logrequest = (req, res, next)=>{
+  console.log(`${new.Date().toLocalString()} Request made to ${req.origninal.url}`);
+  next();//
+}
+//use in total then 
+app.use(localstring);
+app.get('/', function(req, res){
+  res.send('esho modh kabo')
+})
